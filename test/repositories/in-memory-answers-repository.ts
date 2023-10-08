8b525c1c2e0308a5dd7807e151d6fecf786812c9
Luiz Rioja <lulyrioja@gmail.com>
2023-10-08 15:15:16 -0300

    feat: create answers and questions in memory repository

---
+++ test/repositories/in-memory-answers-repository.ts
@@ -0,0 +1,32 @@
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
+import { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+
+export class InMemoryAnswersRepository implements AnswersRepository {
+  public items: Answer[] = []
+
+  async create(answer: Answer) {
+    this.items.push(answer)
+  }
+
+  async findById(id: string) {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
+  async save(answer: Answer): Promise<void> {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items[itemIndex] = answer
+  }
+}
