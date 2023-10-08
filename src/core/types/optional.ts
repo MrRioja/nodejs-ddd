09ab94780e4473413aecfa5d4ad81bf1fd6b5745
Luiz Rioja <lulyrioja@gmail.com>
2023-10-07 23:09:16 -0300

    add optional type

---
+++ src/core/types/optional.ts
@@ -0,0 +1,16 @@
+/**
+ * @description Make some properties of T optional
+ *
+ * @example
+ * ```ts
+ * type Post {
+ *  id: string;
+ *  name: string;
+ *  email: string;
+ * }
+ *
+ * Optional<Post, 'id' | 'email'>
+ * ```
+ */
+
+export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
