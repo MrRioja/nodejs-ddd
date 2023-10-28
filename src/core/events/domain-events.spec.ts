32247f3945d7e233812f7a0e02c0e71ed3704a71
Luiz Rioja <lulyrioja@gmail.com>
2023-10-28 17:27:43 -0300

    feat: create domain event interface, implement class and test it

---
+++ src/core/events/domain-events.spec.ts
@@ -0,0 +1,45 @@
+import { DomainEvent } from './domain-event'
+import { DomainEvents } from './domain-events'
+import { AggregateRoot } from '../entities/aggregate-root'
+import { UniqueEntityID } from '../entities/unique-entity-id'
+
+class CustomAggregateCreated implements DomainEvent {
+  public ocurredAt: Date
+  public aggregate: CustomAggregate // eslint-disable-line
+
+  constructor(aggregate: CustomAggregate) {
+    this.aggregate = aggregate
+    this.ocurredAt = new Date()
+  }
+
+  public getAggregateId(): UniqueEntityID {
+    return this.aggregate.id
+  }
+}
+
+class CustomAggregate extends AggregateRoot<any> {
+  static create() {
+    const aggregate = new CustomAggregate(null)
+
+    aggregate.addDomainEvent(new CustomAggregateCreated(aggregate))
+
+    return aggregate
+  }
+}
+
+describe('Domain Events', () => {
+  it('should be able to dispatch and listen to events', () => {
+    const callbackSpy = vi.fn()
+
+    DomainEvents.register(callbackSpy, CustomAggregateCreated.name)
+
+    const aggregate = CustomAggregate.create()
+
+    expect(aggregate.domainEvents).toHaveLength(1)
+
+    DomainEvents.dispatchEventsForAggregate(aggregate.id)
+
+    expect(callbackSpy).toHaveBeenCalled()
+    expect(aggregate.domainEvents).toHaveLength(0)
+  })
+})
